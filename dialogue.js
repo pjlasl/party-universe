5483f7fbae701fe663508197f02418abee55a5d1
Sief Naber <siefnaber@gmail.com>
2019-02-06 15:55:19 -0800

    Version alpha 0.2
    Added more supplies, console messages, and HTML improvements.

--- dialogue.js
+++ dialogue.js
@@ -1,7 +1,14 @@
+var keyList = [
+	"It's just you here...",
+	"Welcome back to the party!",
+	"You can't afford that!"
+];
+
 var rejectionList = [
+	"I don't know you...",
 	"I don't have a ride!",
-	"I can't, my dog is throwing up.",
-	"Sorry, I have a package I have to sign for.",
+	"My dog is throwing up.",
+	"I have a package I have to sign for.",
 	"I wish but Uber surge pricing is at 5.6x right now!",
 	"It's raining, maybe another time.",
 	"I had a burrito for dinner and now I can't move.",
@@ -9,23 +16,23 @@ var rejectionList = [
 	"I have a migraine, party on without me.",
 	"I can't find my keys.",
 	"I'm too upset about the state of the country to party right now.",
-	"I have terrible diarrhea, I'll stay put.",
-	"There's a flash flood warning in my area, probably best to stay put.",
+	"I have terrible diarrhea.",
+	"There's a flash flood warning in my area.",
 	"I just really don't want to be hungover tomorrow.",
-	"I can't afford to spot some cash and I'd rather not come without a donation.",
 	"Didn't we already party last weekend...?",
 	"I'm just not feeling it this week.",
 	"I started Twitter beef so I'm trying to keep it low key.",
 	"You know the exposed video? That was me. Probably best to lay low.",
 	"Sorry, my nails are still drying.",
-	"I'm dog sitting, unless he can come, too...?",
+	"I'm dog sitting.",
+	"I'm cat sitting.",
+	"I'm baby sitting.",
 	"I found a baby on my doorstep and I have to raise it now.",
-	"I'mma be real with you... I don't feel like putting on pants.",
+	"I don't feel like putting on pants.",
 	"I'd rather be asleep.",
 	"I'd rather be dead.",
 	"My social anxiety is flaring up.",
-	"I have to Facetime my long distance girlfriend, sorry!",
-	"I'm in the middle of a Twitter argument, can we party later?",
+	"I'm in the middle of a Twitter argument, party later?",
 	"Sorry, I've got a doctor's appointment",
 	"I'd love to... But I can't",
 	"Sorry man, I got a thing...",
@@ -38,5 +45,13 @@ var acceptanceList = [
 	"A party? I'm game.",
 	"You know I'm always down for a party.",
 	"I hope the beer is still cold!",
-	"I'm there."
+	"I'm there.",
+	"Is it even a party without me there?"
+];
+
+var noRoomList = [
+	"I can't squeeze in!",
+	"Looks like there's no more room!",
+	"I'd love to, but there's no room!",
+	"Looks like your party is at max capacity"
 ];
--- main.js
+++ main.js
@@ -1,49 +1,181 @@
+// =============================================================
+// Game Data
+// -------------------------------------------------------------
 var gameData = {
 	party: 1,
+	partyCapacity: 50,
 	money: 0.00,
 	clout: 0.00,
 	partyPerInvite: 1,
 	
-	beer: 0,
-	beerCost: 5
-}
+	supplies: {
+		balloon: 0,
+		beer: 0,
+		hardAlcohol: 0,
+		beerPongTable: 0,
+		keg: 0,
+		speaker: 0
+	},
+	
+	cost: {
+		balloonCost: 0.25,
+		beerCost: 4,
+		hardAlcoholCost: 12,
+		beerPongTableCost: 50,
+		kegCost: 100,
+		speakerCost: 300
+	}
+};
+// =============================================================
 
-function inviteSomeone() {
-	if (gameData.clout + 0.1 >= Math.random()) {
+
+// =============================================================
+// Utility functions
+// -------------------------------------------------------------
+function getTimestamp() {
+	var date = new Date();
+	return date.toLocaleTimeString();
+}
+// =============================================================
+
+
+// =============================================================
+// Button functions
+// -------------------------------------------------------------
+function invite() {
+	// Party at capacity
+	if (gameData.party >= gameData.partyCapacity) {
+		updateConsole(chance.name(), noRoomList, Math.floor(Math.random()*noRoomList.length));
+	// Successful invite
+	} else if (gameData.clout + 0.1 >= Math.random()) {
 		gameData.party += gameData.partyPerInvite;
 		gameData.money += (Math.floor(Math.random() * 500))/100 + 1;
 		
 		updateStatDisplay();
 		updateButtons();
-		updateConsole(acceptanceList);
+		updateConsole(chance.name(), acceptanceList, Math.floor(Math.random()*acceptanceList.length));
+	// Unsuccessful invite
 	} else {
-		updateConsole(rejectionList);
+		updateConsole(chance.name(), rejectionList, Math.floor(Math.random()*rejectionList.length));
+	}
+}
+
+function buySupply(item, cost) {
+	console.log(item);
+	
+	if (gameData.money >= cost) {
+		gameData.money -= cost;
+		item += 1;
+		updateAll();
+	} else {
+		updateConsole(chance.phone(), key, 2);
+	}
+}
+
+function buyBalloon() {
+	if (gameData.money >= gameData.cost.balloonCost) {
+		gameData.money -= gameData.cost.balloonCost;
+		gameData.supplies.balloon += 1;
+		updateAll();
+	} else {
+		updateConsole(chance.phone(), key, 2);
 	}
 }
 
 function buyBeer() {
-	if (gameData.money >= gameData.beerCost) {
-		gameData.money -= gameData.beerCost;
-		gameData.beer += 1;
-		updateStatDisplay();
-		updateButtons();
+	if (gameData.money >= gameData.cost.beerCost) {
+		gameData.money -= gameData.cost.beerCost;
+		gameData.supplies.beer += 1;
+		updateAll();
+	} else {
+		updateConsole(chance.phone(), key, 2);
 	}
 }
 
+function buyHardAlcohol() {
+	if (gameData.money >= gameData.cost.hardAlcoholCost) {
+		gameData.money -= gameData.cost.hardAlcoholCost;
+		gameData.supplies.hardAlcohol += 1;
+		updateAll();
+	} else {
+		updateConsole(chance.phone(), key, 2);
+	}
+}
+
+function buyBeerPongTable() {
+	if (gameData.money >= gameData.cost.beerPongTableCost) {
+		gameData.money -= gameData.cost.beerPongTableCost;
+		gameData.supplies.beerPongTable += 1;
+		updateAll();
+	} else {
+		updateConsole(chance.phone(), key, 2);
+	}
+}
+
+function buyKeg() {
+	if (gameData.money >= gameData.cost.kegCost) {
+		gameData.money -= gameData.cost.kegCost;
+		gameData.supplies.keg += 1;
+		updateAll();
+	} else {
+		updateConsole(chance.phone(), key, 2);
+	}
+}
+
+function buySpeaker() {
+	if (gameData.money >= gameData.cost.speakerCost) {
+		gameData.money -= gameData.cost.speakerCost;
+		gameData.supplies.speaker += 1;
+		updateAll();
+	} else {
+		updateConsole(chance.phone(), key, 2);
+	}
+}
+// =============================================================
+
+
+
+// =============================================================
+// Update functions
+// -------------------------------------------------------------
 function updateStatDisplay() {
-	document.getElementById("party").innerHTML = gameData.party + " Party Goers";
+	document.getElementById("party").innerHTML = gameData.party + " / " + gameData.partyCapacity + " Attendees";
 	document.getElementById("money").innerHTML = "$" + gameData.money.toFixed(2);
 	document.getElementById("clout").innerHTML = gameData.clout.toFixed(2) + "% Clout";
 	
-	document.getElementById("beer").innerHTML = gameData.beer + " Beer";
+	document.getElementById("balloon").innerHTML = gameData.supplies.balloon + " Balloons";
+	document.getElementById("beer").innerHTML = gameData.supplies.beer + " Beers";
+	document.getElementById("hardAlcohol").innerHTML = gameData.supplies.hardAlcohol + " Bottles of Hard Alcohol";
+	document.getElementById("beerPongTable").innerHTML = gameData.supplies.beerPongTable + " Beer Pong Tables";
+	document.getElementById("keg").innerHTML = gameData.supplies.keg + " Kegs";
+	document.getElementById("speaker").innerHTML = gameData.supplies.speaker + " Speakers";
+	
+	document.getElementById("balloonUpgrade").innerHTML = "Buy ($" + gameData.cost.balloonCost + ")";
+	document.getElementById("beerUpgrade").innerHTML = "Buy ($" + gameData.cost.beerCost + ")";
+	document.getElementById("hardAlcoholUpgrade").innerHTML = "Buy ($" + gameData.cost.hardAlcoholCost + ")";
+	document.getElementById("beerPongTableUpgrade").innerHTML = "Buy ($" + gameData.cost.beerPongTableCost + ")";
+	document.getElementById("kegUpgrade").innerHTML = "Buy ($" + gameData.cost.kegCost + ")";
+	document.getElementById("speakerUpgrade").innerHTML = "Buy ($" + gameData.cost.speakerCost + ")";
 }
 
 function updateButtons() {
-	if (gameData.money >= gameData.beerCost) {
-		document.getElementById("beerUpgrade").disabled = false;
-	} else {
-		document.getElementById("beerUpgrade").disabled = true;
-	}
+	if (gameData.money >= gameData.cost.balloonCost) document.getElementById("balloonUpgrade").disabled = false;
+	else document.getElementById("balloonUpgrade").disabled = true;
+	
+	if (gameData.money >= gameData.cost.beerCost) document.getElementById("beerUpgrade").disabled = false;
+	else document.getElementById("beerUpgrade").disabled = true;
+	
+	if (gameData.money >= gameData.cost.hardAlcoholCost) document.getElementById("hardAlcoholUpgrade").disabled = false;
+	else document.getElementById("hardAlcoholUpgrade").disabled = true;
+	
+	if (gameData.money >= gameData.cost.beerPongTableCost) document.getElementById("beerPongTableUpgrade").disabled = false;
+	else document.getElementById("beerPongTableUpgrade").disabled = true;
+	
+	if (gameData.money >= gameData.cost.kegCost) document.getElementById("kegUpgrade").disabled = false;
+	else document.getElementById("kegUpgrade").disabled = true;
+	
+	if (gameData.money >= gameData.cost.speakerCost) document.getElementById("speakerUpgrade").disabled = false;
+	else document.getElementById("speakerUpgrade").disabled = true;
 }
 
 function updateClout() {
@@ -51,36 +183,66 @@ function updateClout() {
 	updateStatDisplay();
 }
 
-function updateConsole(type) {
+function updateConsole(name, type, index) {
 	var newItem = document.createElement("LI");
 	
-	if (type == acceptanceList) {
-		newItem.id = "acceptanceMessage";
-	} else if (type == rejectionList) {
-		newItem.id = "rejectionMessage";
-	} else {
-		newItem.id = "normalMessage";
+	switch (type) {
+		case keyList: newItem.id = "keyMessage"; break;
+		case acceptanceList: newItem.id = "acceptanceMessage"; break;
+		case rejectionList: newItem.id = "rejectionMessage"; break;
+		case noRoomList: newItem.id = "noRoomMessage"; break;
+		default: newItem.id = "normalMessage"; break;
 	}
 	
-	var textNode = document.createTextNode(type[Math.floor(Math.random()*type.length)]);
+	var textNode = document.createTextNode(name + ": " + type[index] + " (" + getTimestamp() + ")");
 	newItem.appendChild(textNode);
 	var list = document.getElementById("console");
 	list.insertBefore(newItem, list.childNodes[0]);
 }
 
-var mainGameLoop = window.setInterval(function() {
+function updateAll() {
 	updateStatDisplay();
 	updateButtons();
 	updateClout();
+}
+// =============================================================
+
+
+// Main Game Loop
+var mainGameLoop = window.setInterval(function() {
+	updateAll();
 }, 1000)
 
-/*
-var saveGameLoop = window.setInterval(function() {
-	localStorage.setItem('partyUniverseSave', JSON.stringify(gameData))
-}, 15000)
-
-var gameSave = JSON.parse(localStorage.getItem("partyUniverseSave"))
+// =============================================================
+// Data Management
+// -------------------------------------------------------------
+// Load game on page load
+var gameSave = JSON.parse(localStorage.getItem("partyUniverseSave"));
 if (gameSave !== null) {
 	gameData = gameSave
+	updateConsole(chance.phone(), keyList, 1);
+} else {
+	// New game
+	updateConsole(chance.phone(), keyList, 0);
 }
-*/
+updateAll();
+
+function loadGame() {
+	location.reload();
+}
+
+function saveGame() {
+	localStorage.setItem("partyUniverseSave", JSON.stringify(gameData))
+}
+
+// Autosave every 15 seconds
+var saveGameLoop = window.setInterval(function() {
+	saveGame();
+}, 15000)
+
+// Delete save
+function deleteSave() {
+	localStorage.removeItem("partyUniverseSave");
+	loadGame();
+}
+// =============================================================
